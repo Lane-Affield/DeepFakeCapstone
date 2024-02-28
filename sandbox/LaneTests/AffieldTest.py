476e5bf413a3abde4a31d3c0ad939d81082429f4
LaneAffield <laneaffield@gmail.com>
2024-02-28 11:53:07 -0600

    added sandbox and test files for us to use

---
+++ sandbox/LaneTests/AffieldTest.py
@@ -0,0 +1,9 @@
+'''
+Author(s): Lane Affield
+Created: 02/28/2024
+Last Update: 02/28/2024
+Last Updated by: Lane Affield
+
+Desrciption: Lane Affields Test Ground for The AI Capstone
+
+'''
---
+++ sandbox/MunachiTests/OkuaguTest.py
@@ -0,0 +1,9 @@
+'''
+Author(s):
+Created: 02/28/2024
+Last Update:
+Last Updated by:
+
+Desrciption:
+
+'''
